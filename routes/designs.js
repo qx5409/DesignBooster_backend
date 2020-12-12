575a95135ba88f5e42578084278fe6aa6aa6931c
qx5409 <qx5409@yahoo.be>
2020-12-12 15:14:20 +0100

    add better stockage system

--- routes/designs.js
+++ routes/designs.js
@@ -1,32 +1,25 @@
+"use strict";
 var express = require("express");
 var router = express.Router();
 const jwt = require("jsonwebtoken");
-const listDesign = [
-  {
-    id: 1,
-    name: "name",
-    images: "base64",
-    description: "Super design",
-    feature: "Liste des features",
-    imageIncluded: "true",
-    videoIncluded: "false",
-    firstName: "firstName",
-    lastName: "lastName",
-    facebook: "url facebook",
-    email: "email@mail.com",
-    instagram: "url insta",
-    linkedin: "url linkedin"
-  }
-];
+const Design = require("../model/Design");
 
 const jwtSecret = "secretDesign";
-/* GET /designs : list all the designs from the listDesign */
+
+/* GET /designs : list all the designs from designs.json */
 router.get("/", function (req, res, next) {
-  res.json(listDesign);
+  return res.json(Design.list);
 });
 
-/* POST /designs : create a design to be added to the listDesign 
-Return the new design
+// Read an identified design : GET /api/designs/:id
+router.get("/:id", function (req, res) {
+  const designFound = Design.get(req.params.id);
+  if (!designFound) return res.status(404).end();
+
+  return res.json(designFound);
+});
+
+/* POST /designs : create a design to be added to designs.json
 This ressource operation shall be authorized only to designadmin and protected via a JWT !*/
 router.post("/", function (req, res, next) {
   /* To authorize the access: 
@@ -39,26 +32,25 @@ router.post("/", function (req, res, next) {
     if (err) return res.status(401).send(err.message);
     if (token.username !== "designAdmin")
       return res.status(403).send("You are not authorized.");
-    const newDesign = {
-      id: listDesign.length + 1,
-      name: req.body.name,
-      title: req.body.title,
-      content: req.body.content,
-      images: req.body.images,
-      description: req.body.description,
-      feature: req.body.feature,
-      imageIncluded: req.body.imageIncluded,
-      videoIncluded: req.body.videoIncluded,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      facebook: req.body.facebook,
-      email: req.body.email,
-      instagram: req.body.instagram,
-      linkedin: req.body.linkedin
-    };
-    listDesign.push(newDesign);
+    
+    let newDesign = new Design(req.body);
+    newDesign.save();
     return res.json(newDesign);
   });
+});
+
+// Update a design : PUT /api/designs/:id
+router.put("/:id", function (req, res) {
+  const designUpdated = Design.update(req.params.id, req.body);
+  if (!designUpdated) return res.status(404).end();
+  return res.json(designUpdated);
+});
+
+// Delete a design : DELETE /api/designs/:id
+router.delete("/:id", function (req, res) {
+  const designDeleted = Design.delete(req.params.id);
+  if (!designDeleted) return res.status(404).end();
+  return res.json(designDeleted);
 });
 
 module.exports = router;
